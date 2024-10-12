8ebc953758511dd9b4474fece56ebee86bc2b6c9
Pascasio Krel <ktp9380@students.uc-bcf.edu.ph>
2024-10-12 22:47:53 +0800

    .

--- Activity3.php
+++ Activity3.php
@@ -1,12 +1,5 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Multiplication Table</title>
-</head>
-<body>
- <?php
+<?php
+echo "Multiplication Table\n";
 $n = 7;
 
 for ($i = 1; $i <= 10; $i++) {
@@ -14,5 +7,3 @@ for ($i = 1; $i <= 10; $i++) {
     echo "$n x $i = $result\n";
 }
 ?>
-</body>
-</html>
